6cec41271afaff276db80c38274accb9e6946f31
George Lamb <wglamb196@gmail.com>
2021-08-08 16:23:42 +0100

    finalised orchestration.py

--- first_steps/orchestration.py
+++ first_steps/orchestration.py
@@ -6,15 +6,15 @@
 ###########################################
 # NOTES TO READ BEFORE RUNNING THIS SCRIPT
 # before running this script we need to start and configure Core's server
-# open a new terminal and run the following from the CLI, with our pipenv virtual environment activated:
+# to do this we run the following from the CLI:
 #   prefect backend server
 #   prefect server start
 # after starting and configuring the server, we can navigate to http://localhost:8080 to see the Prefect UI
 # we can then run this script to build a flow, and register it with the local backend
 # after registering the flow we can:
 # - use the URL returned from the register() call to navigate directly to the flow in the UI
-# - start a local agent by running 'prefect agent local start' in the CLI, with our pipenv virtual environment activated
-# - (the local agent communicates between the server and this flow code)
+# - start a local agent by running 'prefect agent local start' in the CLI
+# - (the local agent communicates between the server and the flow code)
 # - trigger a flow run from the UI by pressing the 'Run' button
 ###########################################
 
@@ -23,12 +23,11 @@ from prefect import Flow, Parameter, Client
 
 # first we build a flow
 
-second_add = add.copy()
 with Flow('flow for orchestration') as flow:
     name = Parameter('name')
-    first_result = add(1, y=2)
-    second_result = second_add(x=first_result, y=100)
-    say_hello(person=name, upstream_tasks=[second_add])
+    first_result = add(x=1, y=2)
+    second_result = add(x=first_result, y=100)
+    say_hello(person=name, upstream_tasks=[second_result])
 
 # now we register the flow
 
