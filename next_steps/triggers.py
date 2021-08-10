f557e58026c77ccc7310f0ec9dc3dc3ea0100521
George Lamb <wglamb196@gmail.com>
2021-08-10 09:34:15 +0100

    triggers done

---
+++ next_steps/triggers.py
@@ -0,0 +1,44 @@
+# TRIGGERS
+# normal data pipeline rules: when a task succeeds, its downstream dependencies start running
+# more complex requirement: a 'clean up task' that must run even if a prior task has failed
+# solution is a concept called triggers:
+# - before a task runs, its trigger function is called on a set of upstream task states
+# - task runs if trigger function passes
+# - Prefect has built in triggers: all_successful (default), all_failed, any_successful, any_field, manual_only
+
+from prefect import task, Flow, Parameter, triggers
+
+# define tasks
+
+@task
+def create_cluster(name):
+    return name+'\'s spark cluster'
+
+@task
+def run_spark_job(cluster):
+    #print(cluster+' with job')
+    print(cluster+4) # simulating a failed job
+
+@task(trigger=triggers.always_run) # use the always_run trigger, an alias for all_finished
+def tear_down_cluster(cluster):
+    print(cluster+' torn down')
+
+# define flow
+
+with Flow("Spark") as flow:
+
+    # define data dependencies
+    name = Parameter('name')
+    cluster = create_cluster(name)
+    submitted = run_spark_job(cluster)
+    result = tear_down_cluster(cluster)
+
+    # wait for the job to finish before tearing down the cluster
+    result.set_upstream(submitted)
+
+flow.run(name='Simon')
+
+# note: a trigger function evaluates both data and non-data dependencies
+# for example, if the create_cluster task above were to fail:
+# - run_spark_job would have final state 'TriggerFailed'
+# - tear_down_cluster could have final state 'Failed'
